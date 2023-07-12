6a88f42bf1cc5eac47d6e80bb16d47d1bebb3105
gaurav-singh-panwar <55618776+gaurav-singh-panwar@users.noreply.github.com>
2023-07-12 12:55:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function fibonacci(num) {
 // your code here
+	if (typeof num !== 'number' || !Number.isInteger(num) || num < 0) {
+    throw new Error('Input must be a non-negative integer.');
+  }
+  
+  // base cases
+  if (num === 0) {
+    return 0;
+  } else if (num === 1) {
+    return 1;
+  }
+  
+  // recursive case
+  return fibonacci(num - 1) + fibonacci(num - 2);
 }
 
 module.exports = fibonacci;
