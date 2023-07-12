e1f24c3aa1b43a05b8030ae3bcccc2034d0a2f7e
gaurav-singh-panwar <55618776+gaurav-singh-panwar@users.noreply.github.com>
2023-07-12 13:10:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,20 @@
 function fibonacci(num) {
-  if (typeof num !== "number" || !Number.isInteger(num) || num < 0 || num > 50) {
-    throw new Error("Input must be a non-negative integer between 0 and 50.");
+  if (
+    typeof num !== "number" ||
+    !Number.isInteger(num) ||
+    num < 0 ||
+    num > 50
+  ) {
+    throw new Error(
+      "Input must be a non-negative integer between 0 and 50."
+    );
   }
 
-  if (num === 0) {
-    return 0;
-  } else if (num === 1) {
-    return 1;
+  if (num < 2) {
+    return num;
   }
 
-  let prev = 0;
-  let current = 1;
-
-  for (let i = 2; i <= num; i++) {
-    let next = prev + current;
-    prev = current;
-    current = next;
-  }
-
-  return current;
+  return fibonacci(num - 1) + fibonacci(num - 2);
 }
 
 module.exports = fibonacci;
